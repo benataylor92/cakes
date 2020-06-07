ca57b24b31dffa1fc3b572de2db8605f77315b93
Ben Taylor <TAYLORBX@legal.regn.net>
2020-06-07 17:46:58 +0100

    added header and footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Footer/footer.css';
+
+function Footer() {
+    return (
+        <div className="Test1">Test</div>
+    )
+}
+
+export default Footer;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Header/header.css';
+
+function Header() {
+    return (
+        <div className="Test">Test</div>
+    )
+}
+
+export default Header;
