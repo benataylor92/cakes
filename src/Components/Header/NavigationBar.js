d5108e6e3e2bcc1801b1bb7e469400c6f74486b0
Ben Taylor <TAYLORBX@legal.regn.net>
2020-06-13 12:43:35 +0100

    added reset css file and header navigation

--- src/Components/Header/NavigationBar.js
+++ src/Components/Header/NavigationBar.js
@@ -3,8 +3,15 @@ import '../Header/navigationBar.css'
 
 function NavigationBar() {
     return (
-        <div className="Header_Banner">
-            Hello
+        <div className="Navigation_Banner">
+            <nav>
+                <ul className="Buy_A_Cake">
+                    <li><a href="/buyacake">Buy a Cake</a></li>
+                </ul>
+                <ul className="Become_A_Baker">
+                    <li><a href="/buyacake">Become a Baker</a></li>
+                </ul>
+            </nav>
         </div>
     )
 }
