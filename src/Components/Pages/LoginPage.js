935431ff28b2acdd8afd0996fedc0f00c86df441
Ben Taylor <TAYLORBX@legal.regn.net>
2020-06-18 09:38:46 +0100

    test

--- src/Components/Pages/LoginPage.js
+++ src/Components/Pages/LoginPage.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import '../Pages/loginPage.css';
 
 function LoginPage() {
-    const [emailaddress, updateEmailAddress ] = useState();
-    const [password, updatePassword ] = useState();
+    const [emailaddress, setEmailAddress ] = useState();
+    const [password, setPassword ] = useState();
+
+    const handleEmailAddressChange = event => setEmailAddress(event.target.value)
+    const handlePasswordChange = event => setPassword(event.target.value)
 
     return (
         <div className="Login-component">   
@@ -11,10 +14,10 @@ function LoginPage() {
             <form className="Login-form">
                 <div className="Form-group">
                     <label htmlFor="Email-name">Email Adress</label>
-                    <input type="text" className="Form-control" name="emailAdress" value={emailaddress} />
+                    <input type="text" className="Form-control" name="emailAdress" value={emailaddress} onChange={handleEmailAddressChange} />
                 <div>
                     <label htmlFor="Password">Password</label>
-                    <input type="text" className="Form-control" name="password" value={password} />
+                    <input type="text" className="Form-control" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
                 <button className="Form-button">Login</button>
