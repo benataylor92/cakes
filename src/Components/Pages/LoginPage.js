d076bcfcc45b85597b9816554094bb40737054f6
benataylor92 <benataylor1992@hotmail.co.uk>
2020-08-25 15:16:49 +0100

    updated login page with AWS Cognitp

--- src/Components/Pages/LoginPage.js
+++ src/Components/Pages/LoginPage.js
@@ -1,32 +1,52 @@
 import React, { useState } from 'react';
-import '../Pages/loginPage.css';
+import UserPool from '../Authentication/UserPool';
+import { CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
 
-function LoginPage() {
-    const [emailaddress, setEmailAddress ] = useState();
-    const [password, setPassword ] = useState();
+export default () => {
+    const [emailaddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleEmailAddressChange = event => setEmailAddress(event.target.value)
-    const handlePasswordChange = event => setPassword(event.target.value)
+    const onSubmit = event => {
+        event.preventDefault();
+
+        const user = new CognitoUser({
+            Username: emailaddress,
+            Pool: UserPool
+        });
+
+        const authDetails = new AuthenticationDetails({
+            Username: emailaddress,
+            Password: password
+        });
+
+        user.authenticateUser(authDetails, {
+            onSuccess: data => {
+                console.log('onSuccess:', data);
+            },
+
+            onFailure: err => {
+                console.error('onFailure:', err );
+            },
+
+            newPasswordRequired: data => {
+                console.log('newPasswordRequired:', data );
+            }
+        });
+    };
 
     return (
-        <div className="Login-component">   
-            <div className="Login1">Login</div>
-            <form className="Login-form">
-                <div className="Form-group">
-                    <label htmlFor="Email-name">Email Adress</label>
-                    <input type="text" className="Form-control" name="emailAdress" value={emailaddress} onChange={handleEmailAddressChange} />
-                <div>
-                    <label htmlFor="Password">Password</label>
-                    <input type="text" className="Form-control" name="password" value={password} onChange={handlePasswordChange}/>
-                </div>
-                <div>
-                <button className="Form-button">Login</button>
-                <a className="Register-button" href="/Signup">Sign Up</a>
-                </div>
-                </div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    value={emailaddress}
+                    onChange={event => setEmailAddress(event.target.value)}
+                />
+                <input 
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                />
+                <button type='submit'>Login</button>
             </form>
         </div>
-    );
-}
-
-export default LoginPage;
+    )
+};
